ab2048712c29dc294af351a08cce1173b3e54962
Rohit Sahoo <rohitsahoo112003@gmail.com>
2025-11-26 10:56:37 +0530

    product service

---
+++ product-service/src/test/java/com/rohit/product_service/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rohit.product_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
