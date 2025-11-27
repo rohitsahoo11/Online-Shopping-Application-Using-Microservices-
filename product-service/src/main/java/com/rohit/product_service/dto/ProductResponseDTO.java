c9f36e78528234ccb3c394b403d505560dd627ea
Rohit Sahoo <rohitsahoo112003@gmail.com>
2025-11-27 17:50:04 +0530

    Product Service

---
+++ product-service/src/main/java/com/rohit/product_service/dto/ProductResponseDTO.java
@@ -0,0 +1,58 @@
+package com.rohit.product_service.dto;
+
+import java.math.BigDecimal;
+
+public class ProductResponseDTO {
+	
+	private Integer id;
+	private String name;
+	private String description;
+	private BigDecimal price;
+	
+	public ProductResponseDTO() {
+		super();
+	}
+
+	public ProductResponseDTO(Integer id, String name, String description, BigDecimal price) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.price = price;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public BigDecimal getPrice() {
+		return price;
+	}
+
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+	
+	
+	
+}
