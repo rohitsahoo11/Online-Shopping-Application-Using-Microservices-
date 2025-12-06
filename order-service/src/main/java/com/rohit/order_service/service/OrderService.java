745da7d7d48c2bec3125f48e259301f15b32ff08
Rohit Sahoo <rohitsahoo112003@gmail.com>
2025-12-06 23:31:10 +0530

    Adding Feign client

--- order-service/src/main/java/com/rohit/order_service/service/OrderService.java
+++ order-service/src/main/java/com/rohit/order_service/service/OrderService.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.rohit.order_service.client.InventoryClient;
 import com.rohit.order_service.dto.OrderRequestDTO;
 import com.rohit.order_service.dto.OrderResponseDTO;
 import com.rohit.order_service.model.Order;
@@ -16,26 +17,39 @@ public class OrderService {
 	@Autowired
 	private OrderRepository repo;
 	
+	@Autowired
+	private InventoryClient client;
+	
 	
 	public OrderResponseDTO placeOrder(OrderRequestDTO dto) {
 		
-		Order order = new Order();
+		boolean isAvailabel = client.isInStock(dto.getSkuCode(), dto.getQuantity());
 		
-		order.setOrderNumber(UUID.randomUUID().toString());
-		order.setPrice(dto.getPrice());
-		order.setQuantity(dto.getQuantity());
-		order.setSkuCode(dto.getSkuCode());
+		if(isAvailabel) {
+			Order order = new Order();
+			
+			order.setOrderNumber(UUID.randomUUID().toString());
+			order.setPrice(dto.getPrice());
+			order.setQuantity(dto.getQuantity());
+			order.setSkuCode(dto.getSkuCode());
+			
+			Order saved = repo.save(order);
+			
+			
+			return new OrderResponseDTO(
+						saved.getId(),
+						saved.getOrderNumber(),
+						saved.getSkuCode(),
+						saved.getPrice(),
+						saved.getQuantity()
+					);
+
+		}
+		else {
+			throw new RuntimeException("Order with the sku code not available.");
+		}
 		
-		Order saved = repo.save(order);
-		
-		return new OrderResponseDTO(
-					saved.getId(),
-					saved.getOrderNumber(),
-					saved.getSkuCode(),
-					saved.getPrice(),
-					saved.getQuantity()
-				);
-		
+				
 	}
 	
 }
